a471de58df4bcc9556f7b20e80d77cf4468cc89a
John Allen Lorena <johnallenlorena@gmail.com>
2023-04-28 19:12:39 +0800

    Odd and Even numbers are separated

--- oddeven.py
+++ oddeven.py
@@ -5,6 +5,9 @@ def oddeven():
         #Convert string to integer
         for line in input_file:
             integer = int(line)
-#Separate odd and even numbers
+            #Separate odd and even numbers
+            if integer % 2 == 0:
+                output_even.write(str(integer) + "\n")
+            else:
+                output_odd.write(str(integer) + "\n")
 #Start Program
-
