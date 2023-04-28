90acd1e93e7294eafb20d2963eda87a33a94bc72
John Allen Lorena <johnallenlorena@gmail.com>
2023-04-28 19:08:24 +0800

    Method for opening text files added

--- oddeven.py
+++ oddeven.py
@@ -1,6 +1,7 @@
 #Create a function
 def oddeven():
-#Open text file containing the numbers and output text files for separation
+    #Open text file containing the numbers and output text files for separation
+    with open("twenty.txt") as input_file, open("odd.txt", "a") as output_odd, open("even.txt", "a") as output_even:
 #Convert string to integer
 #Separate odd and even numbers
 #Start Program
